1bb09fdcbffcfe893a6ae09a96cd57d6485cfbaf
nadyasyantikaa <nadiasantikanaraya.com>
2024-09-06 10:45:49 +0700

    First Github Project

---
+++ luas_segitiga.java
@@ -0,0 +1,14 @@
+public class luas_segitiga {
+    public static void main (String[] args) {
+        // deklarasi variabel
+        double alas, tinggi, luas;
+
+        // inisialisasi variabel
+        alas=5;
+        tinggi=10;
+        luas=0.5*alas*tinggi;
+
+        //cetak hasil
+        System.out.println("luas segitiga adalah"+luas);
+    }
+}
